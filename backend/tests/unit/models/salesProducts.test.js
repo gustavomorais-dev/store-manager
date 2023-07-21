79d7db5917513004975cff18e470dd4530b3a289
gustavomorais <gustavomorais.dev@gmail.com>
2023-07-21 11:15:46 -0300

    adiciona mais testes para o salesproducts model

--- backend/tests/unit/models/salesProducts.test.js
+++ backend/tests/unit/models/salesProducts.test.js
@@ -19,6 +19,16 @@ describe('Testes do SALES MODEL:', function () {
     expect(rowsAffected).to.equal(affectedRows);
   });
 
+  it('Deleta corretamente uma venda na database', async function () {
+    const saleId = 1;
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 2 }]);
+
+    const result = await salesProductsModel.deleteById(saleId);
+
+    expect(result).to.be.an('object');
+    expect(result).to.be.deep.equal({ saleId });
+  });
+
   afterEach(function () {
     sinon.restore();
   });
